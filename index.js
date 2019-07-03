5cc0361bf133aaf6ee8e5556d474daba8f39fa12
Adolfo Reyna <aeroreyna@gmail.com>
2019-07-03 12:52:19 -0700

    add build and ignore references if not bibliography meta is present

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ var pandoc = require('pandoc-filter-promisified');
 const doi2bib = require('doi2bib');
 var Str = pandoc.Str;
 var libraryOpen = false;
+var noBib = false;
 
 async function action(elt,format,meta) {
     if (elt.t === 'Cite'){
-      if(!libraryOpen) doi2bib.setLibraryFile(meta['bibliography'].c[0].c)
+      if(!libraryOpen){
+        if(meta['bibliography']) doi2bib.setLibraryFile(meta['bibliography'].c[0].c)
+        else noBib = true
+        libraryOpen = true
+      }
       p = [];
-      elt.c[0].forEach((c)=>{
-        if(c.citationId && c.citationId.indexOf('DOI:') != -1){
-          p.push(doi2bib.getCitation(c.citationId.substring(4)));
-        }
-      })
+      if(!noBib){
+        elt.c[0].forEach((c)=>{
+          if(c.citationId && c.citationId.indexOf('DOI:') != -1){
+            p.push(doi2bib.getCitation(c.citationId.substring(4)));
+          }
+        })
+      }
       await Promise.all(p)
     }
 }
